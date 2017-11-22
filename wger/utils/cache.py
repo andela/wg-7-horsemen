b28afd9a4ffff152503fc9d1e3daa38fcd9bc7ad
David Parseen Maitoyo <david.maitoyo@andela.com>
2017-11-22 14:02:49 +0300

    [Feature #[152919271]] -- Added a condition to check for cached result before recalculating and setting cache when and if calculation is done -- Added cache_mapper function for getting specific nutrition_info_cache by id Please enter the commit message for your changes. Lines starting  Untracked files:

--- wger/utils/cache.py
+++ wger/utils/cache.py
@@ -115,6 +115,11 @@ class CacheKeyMapper(object):
         Return the workout canonical representation
         '''
         return self.WORKOUT_LOG_LIST.format(hash_value)
+    def get_nutrition_info_key(self, param):
+        '''
+        Return the Nutrition info cache key
+        '''
+        return self.NUTRITION_INFO_CACHE_KEY.format(self.get_pk(param))
 
     def get_nutrition_info_key(self, param):
         '''
