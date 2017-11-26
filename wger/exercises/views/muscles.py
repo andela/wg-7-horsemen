826c8408810899e0ee0675a7a2bdd13cb97ce3be
Brian Sserumaga <briansserumaga@Your-Girlfriends-bestfriend.local>
2017-11-26 23:53:43 +0300

    Fix cache issue with the exercises.

--- wger/exercises/views/muscles.py
+++ wger/exercises/views/muscles.py
@@ -16,6 +16,7 @@
 import logging
 
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from django.core.cache import cache
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.utils.translation import ugettext as _
 from django.utils.translation import ugettext_lazy
@@ -27,7 +28,8 @@ from django.views.generic import (
     UpdateView
 )
 
-from wger.exercises.models import Muscle
+from wger.exercises.models import Muscle, Exercise
+from wger.utils.cache import cache_mapper
 from wger.utils.generic_views import (
     WgerFormMixin,
     WgerDeleteMixin
@@ -117,4 +119,7 @@ class MuscleDeleteView(WgerDeleteMixin, LoginRequiredMixin, PermissionRequiredMi
         context = super(MuscleDeleteView, self).get_context_data(**kwargs)
         context['form_action'] = reverse('exercise:muscle:delete', kwargs={'pk': self.kwargs['pk']})
         context['title'] = _(u'Delete {0}?').format(self.object.name)
+        exercise_id = 0
+        exercises = Exercise.objects.all()
+        cache.clear()
         return context
