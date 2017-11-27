100fa4e1e146f05d48fa30290c86be9c0726490e
Chris-Maina <krismaish34@gmail.com>
2017-11-27 17:59:29 +0300

    [Chore #152919269] Add settings to load static files

--- settings.py
+++ settings.py
@@ -12,6 +12,13 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
+# load static files
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # DATABASES = {
 #     'default': {
