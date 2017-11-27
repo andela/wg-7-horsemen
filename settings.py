8ade20e386115a9a1038eb4711dfb55cb862c080
Chris-Maina <krismaish34@gmail.com>
2017-11-27 19:20:23 +0300

    [Chore #152919269] Add static files dirs value

--- settings.py
+++ settings.py
@@ -17,6 +17,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/wger/static/'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 DATABASES = {
     'default': {
