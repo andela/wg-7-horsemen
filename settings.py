39dd195999dc3a553538b268dd84c1095a11f477
Chris-Maina <krismaish34@gmail.com>
2017-11-27 18:13:46 +0300

    [Chore #152919269] Refactor static url value

--- settings.py
+++ settings.py
@@ -14,11 +14,9 @@ MANAGERS = ADMINS
 
 # load static files
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/wger/static/'
 
 # DATABASES = {
 #     'default': {
