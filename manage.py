38a6a597e1dc8d9ae11ad7836f107a09b524b11a
Chris-Maina <krismaish34@gmail.com>
2017-11-28 13:53:12 +0300

    [Chore #152919269] Refactor path for settings.py file

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys, os
+import sys
 
 from django.core.management import execute_from_command_line
 
@@ -8,13 +8,11 @@ from tasks import (
     get_user_config_path
 )
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 if __name__ == "__main__":
 
     # If user passed the settings flag ignore the default wger settings
     if not any('--settings' in s for s in sys.argv):
-        setup_django_environment(os.path.join(BASE_DIR, "wg-7-horsemen/wger/settings.py"))
+        setup_django_environment(get_user_config_path('wger', 'settings.py'))
 
     # Alternative to above
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
