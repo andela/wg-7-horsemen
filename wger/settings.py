d8211c0e62f3b2453cffc6938dbc1d85732854d7
Chris-Maina <krismaish34@gmail.com>
2017-11-28 13:47:43 +0300

    [Chore #152919269] Refactor comments to add whitespace

--- wger/settings.py
+++ wger/settings.py
@@ -51,7 +51,7 @@ MEDIA_URL = '/media/'
 ALLOWED_HOSTS = '*'
 
 # This might be a good idea if you setup memcached
-#SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
 # Configure a real backend in production
 if DEBUG:
@@ -61,4 +61,4 @@ if DEBUG:
 WGER_SETTINGS['EMAIL_FROM'] = 'wger Workout Manager <wger@example.com>'
 
 # Your twitter handle, if you have one for this instance.
-#WGER_SETTINGS['TWITTER'] = ''
+# WGER_SETTINGS['TWITTER'] = ''
