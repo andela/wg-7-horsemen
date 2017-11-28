6166fada57c36941ba9223a814ed122671e21b8f
Chris-Maina <krismaish34@gmail.com>
2017-11-28 12:45:17 +0300

    [Chore #152919269] Remove whitespace after (

--- wger/exercises/views/exercises.py
+++ wger/exercises/views/exercises.py
@@ -190,7 +190,7 @@ class ExercisesEditAddView(WgerFormMixin):
                           'license_author']
 
             class Media:
-                js = ( 'https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.7.3/\
+                js = ('https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.7.3/\
                     jquery.tinymce.min.js',)
 
         return ExerciseForm
