28fe2ae109ec9c39c1bcc6408256310dac45a737
Innocent Asiimwe <innocent@Innocents-MacBook-Pro.local>
2017-12-03 11:47:33 +0300

    Correct indent and spacing in fitbit class

--- wger/core/views/fitbit.py
+++ wger/core/views/fitbit.py
@@ -16,107 +16,103 @@ class FitBit:
     AUTHORIZE_URI = 'https://www.fitbit.com/oauth2/authorize'
     TOKEN_REQUEST_URI = 'https://api.fitbit.com/oauth2/token'
 
+    def ComposeAuthorizationuri(self):
+        """Method helps to compose authorization uri with the intended params"""
 
-def ComposeAuthorizationuri(self):
-    """Method helps to compose authorization uri with the intended params"""
+        # parameters for authorization
+        params = {
+            'client_id': self.CLIENT_ID,
+            'response_type': 'code',
+            'scope': self.SCOPE,
+            'redirect_uri': self.REDIRECT_URI
+        }
 
-    # parameters for authorization
-    params = {
-        'client_id': self.CLIENT_ID,
-        'response_type': 'code',
-        'scope': self.SCOPE,
-        'redirect_uri': self.REDIRECT_URI
-    }
+        # encode the parameters
+        urlparams = urllib.parse.urlencode(params)
+        # construct and return authorization_uri
+        return self.AUTHORIZE_URI + '?' + urlparams
 
-    # encode the parameters
-    urlparams = urllib.parse.urlencode(params)
-    # construct and return authorization_uri
-    return self.AUTHORIZE_URI + '?' + urlparams
+    def RequestAccessToken(self, code):
+        """Method to get exchange access_code with access token from fitbits"""
 
+        # Authentication header
+        client_id = self.CLIENT_ID.encode('utf-8')
+        secret = self.CLIENT_SECRET.emcode('utf-8')
+        auth_code = base64.b64encode(client_id + b':' + secret)
+        headers = {
+            'Authorization': 'Basic ' + auth_code,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
 
-def RequestAccessToken(self, code):
-    """Method to get exchange access_code with access token from fitbits"""
+        # parameters for requesting tokens 
+        params = {
+            'code': code,
+            'grant_type': 'authorization_code',
+            'client_id': client_id,
+            'redirect_uri': self.REDIRECT_URI
+        }
 
-    # Authentication header
-    client_id = self.CLIENT_ID.encode('utf-8')
-    secret = self.CLIENT_SECRET.emcode('utf-8')
-    auth_code = base64.b64encode(client_id + b':' + secret)
-    headers = {
-        'Authorization': 'Basic ' + auth_code,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
+        # request for token
+        response = requests.post(
+            self.TOKEN_REQUEST_URI,
+            data=params,
+            headers=headers)
 
-    # parameters for requesting tokens 
-    params = {
-        'code': code,
-        'grant_type': 'authorization_code',
-        'client_id': client_id,
-        'redirect_uri': self.REDIRECT_URI
-    }
+        if response.status_code != 200:
+            raise Exception("Action unsuccessful")
 
-    # request for token
-    response = requests.post(
-        self.TOKEN_REQUEST_URI,
-        data=params,
-        headers=headers)
+        # get the tokens
+        token = dict()
+        token['access_token'] = response.access_token
+        token['refresh_token'] = response.refresh_token
 
-    if response.status_code != 200:
-        raise Exception("Action unsuccessful")
+        return token
 
-    # get the tokens
-    token = dict()
-    token['access_token'] = response.access_token
-    token['refresh_token'] = response.refresh_token
+    def RefreshToken(self, token):
+        """ Refresh expired access token """
 
-    return token
+        # authentication header
+        client_id = self.CLIENT_ID.encode('utf-8')
+        secret = self.CLIENT_SECRET.emcode('utf-8')
+        auth_code = base64.b64encode(client_id + b':' + secret)
+        headers = {
+            'Authorization': 'Basic ' + auth_code,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
 
+        # parameters for refresh token request
+        params = {
+            'grant_type': 'refresh_token',
+            'refresh_token': token['refresh_token']
+        }
 
-def RefreshToken(self, token):
-    """ Refresh expired access token """
+        # request for token
+        response = requests.post(self.TOKEN_REQUEST_URL, data=params, headers=headers)
 
-    # authentication header
-    client_id = self.CLIENT_ID.encode('utf-8')
-    secret = self.CLIENT_SECRET.emcode('utf-8')
-    auth_code = base64.b64encode(client_id + b':' + secret)
-    headers = {
-        'Authorization': 'Basic ' + auth_code,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
+        if response.status_code != 200:
+            raise Exception("Action unsuccessful")
 
-    # parameters for refresh token request
-    params = {
-        'grant_type': 'refresh_token',
-        'refresh_token': token['refresh_token']
-    }
+        # replace tokens 
+        token['access_token'] = response.access_token
+        token['refresh_token'] = response.refresh_token
 
-    # request for token
-    response = requests.post(self.TOKEN_REQUEST_URL, data=params, headers=headers)
+        return token
 
-    if response.status_code != 200:
-        raise Exception("Action unsuccessful")
+    def GetWeight(self, token):
+        """Method makes call to API"""
 
-    # replace tokens 
-    token['access_token'] = response.access_token
-    token['refresh_token'] = response.refresh_token
+        headers = {
+            'Authorization': 'Bearer ' + token['access_token']
+        }
 
-    return token
+        url = 'https://api.fitbit.com/1/user/-/body/weight/date/today/30d.json' + endpoint
 
+        response = requests.get(url, headers=headers)
 
-def GetWeight(self, token):
-    """Method makes call to API"""
-
-    headers = {
-        'Authorization': 'Bearer ' + token['access_token']
-    }
-
-    url = 'https://api.fitbit.com/1/user/-/body/weight/date/today/30d.json' + endpoint
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        return response
-    elif response.status_code == 401:
-        token = self.RefreshToken(token)
-        self.GetWeight(token, endpoint)
-    else:
-        raise Exception("Action unsuccessful")
+        if response.status_code == 200:
+            return response
+        elif response.status_code == 401:
+            token = self.RefreshToken(token)
+            self.GetWeight(token, endpoint)
+        else:
+            raise Exception("Action unsuccessful")
