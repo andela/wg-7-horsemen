93d393ac66fef2fed3fc1c3e998dd73f84dafe46
Chris-Maina <krismaish34@gmail.com>
2017-12-05 15:54:34 +0300

    [Bug] Add a trailing / on STATICFILES_DIRS value.

--- wger/settings.py
+++ wger/settings.py
@@ -21,7 +21,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/wger/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, '/wger/core/static'),
+    os.path.join(PROJECT_ROOT, '/wger/core/static/'),
 )
 
 DATABASES = {
