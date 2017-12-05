0981713942e9816a7d3e9a1be4310bb58db41d6c
Chris-Maina <krismaish34@gmail.com>
2017-12-05 10:23:38 +0300

    [Bug] Add STATICFILES_DIRS to point to extra places to get static files.

--- wger/settings.py
+++ wger/settings.py
@@ -19,6 +19,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/wger/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, '/wger/core/static'),
+)
 
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
